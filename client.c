8cb0ead2dd0a4a718e785c1c8a3d4fe8bc9d1c41
tranngocson210599 <dp1.tranngocson99@gmail.com>
2020-09-19 14:58:11 +0700

    Add client.c tcp

--- client.c
+++ client.c
@@ -1,32 +1,66 @@
-// Client side C/C++ program to demonstrate Socket programming 
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h> 
 #include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
+#include <stdlib.h> 
 #include <string.h> 
-#define PORT 8080 
-
-int main(int argc, char const *argv[]) 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8081 
+#define SA struct sockaddr 
+void func(int sockfd) 
 { 
-	int sock = 0, valread; 
-	struct sockaddr_in serv_addr; 
-	char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
+	char buff[MAX]; 
+	int n; 
+	for (;;) { 
+		bzero(buff, sizeof(buff)); 
+		printf("Enter the string : "); 
+		n = 0; 
+		while ((buff[n++] = getchar()) != '\n') 
+			; 
+		write(sockfd, buff, sizeof(buff)); 
+		bzero(buff, sizeof(buff)); 
+		read(sockfd, buff, sizeof(buff)); 
+		printf("From Server : %s", buff); 
+		if ((strncmp(buff, "exit", 4)) == 0) { 
+			printf("Client Exit...\n"); 
+			break; 
+		} 
+	} 
+} 
 
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
-	
-	// Convert IPv4 and IPv6 addresses from text to binary form 
-	inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr); 
-	 
+int main() 
+{ 
+	int sockfd, connfd; 
+	struct sockaddr_in servaddr, cli; 
 
-	connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	valread = read( sock , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	return 0; 
+	// socket create and varification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	 servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("10.230.5.186"); 
+    servaddr.sin_port = htons(PORT); 
+
+	// connect the client socket to server socket 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+		printf("connection with the server failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("connected to the server..\n"); 
+
+	// function for chat 
+	func(sockfd); 
+
+	// close the socket 
+	close(sockfd); 
 } 
