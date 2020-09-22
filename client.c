c6b2f3a3b19fce28d38ff485304e78c484d066ca
tranngocson210599 <dp1.tranngocson99@gmail.com>
2020-09-22 17:20:56 +0700

    Add 2 file

--- client.c
+++ client.c
@@ -9,24 +9,87 @@
 #define MAX 80 
 #define PORT 8081 
 #define SA struct sockaddr 
+
+struct Task
+{
+    int mType;
+    int tType;
+    int cCnt;
+    int* cId;
+    char data[128];
+};
+unsigned char * serialize_int(unsigned char *buffer, int value)
+{
+    buffer[0] = value >> 24;
+    buffer[1] = value >> 16;
+    buffer[2] = value >> 8;
+    buffer[3] = value;
+    return buffer + 4;
+}
+
+unsigned char * serialize_char(unsigned char *buffer, char value)
+{
+    buffer[0] = value;
+    return buffer + 1;
+}
+
+int deserialize_int(unsigned char *buffer)
+{
+    int value = 0;
+
+    value |= buffer[0] << 24;
+    value |= buffer[1] << 16;
+    value |= buffer[2] << 8;
+    value |= buffer[3];
+    return value;
+
+}
+
+char deserialize_char(unsigned char *buffer)
+{
+    return buffer[0];
+}
+
+unsigned char* serializeTask(unsigned char* msg, const  struct Task* t)
+{
+    msg = serialize_int(msg,t->mType);
+    msg = serialize_int(msg,t->tType);
+    msg = serialize_int(msg,t->cCnt);
+    for(int i=0; i<t->cCnt; i++)
+            msg = serialize_int(msg,t->cId[i*4]);
+
+for(int i=0; i<strlen(t->data); i++)
+    msg = serialize_char(msg,t->data[i]);
+
+    return msg;
+}
+
 void func(int sockfd) 
 { 
-	char buff[MAX]; 
+	//char buff[MAX]; 
+	char * buff;
 	int n; 
-	for (;;) { 
+	struct Task* t;
+	t->cCnt=1;
+	//t->cId=2;
+	t->mType=3;
+	t->data[0]='s';
+	t->cId[0]=5;
+//	for (;;) { 
 		bzero(buff, sizeof(buff)); 
-		printf("Enter the string : "); 
+		printf("Message to server : "); 
 		n = 0; 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
+
+	//	while ((buff[n++] = getchar()) != '\n') ; 
+	    
 		write(sockfd, buff, sizeof(buff)); 
 		bzero(buff, sizeof(buff)); 
 		read(sockfd, buff, sizeof(buff)); 
 		printf("From Server : %s", buff); 
 		if ((strncmp(buff, "exit", 4)) == 0) { 
 			printf("Client Exit...\n"); 
-			break; 
-		} 
+//			break; 
+//		} 
 	} 
 } 
 
@@ -48,6 +111,7 @@ int main()
 	// assign IP, PORT 
 	 servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = inet_addr("10.230.5.186"); 
+     
     servaddr.sin_port = htons(PORT); 
 
 	// connect the client socket to server socket 
@@ -58,8 +122,18 @@ int main()
 	else
 		printf("connected to the server..\n"); 
 
+// struct Task* t;
+// 	t->cCnt=1;
+// 	//t->cId=2;
+// 	t->mType=3;
+// 	t->data[0]='s';
+// 	t->cId[0]=5;
+// 	char * buff;
+// 	buff=serializeTask(buff,t);
+// 		for(int i=0;i<sizeof(buff)/sizeof(char);i++)
+// 		printf("%d",buff[i]);
 	// function for chat 
-	func(sockfd); 
+	//func(sockfd); 
 
 	// close the socket 
 	close(sockfd); 
--- server.c
+++ server.c
@@ -1,56 +1,69 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
 #include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
+#include <netdb.h> 
 #include <netinet/in.h> 
+#include <stdlib.h> 
 #include <string.h> 
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#define MAX 80 
+#define PORT 8081 
+#define SA struct sockaddr 
+#include <unistd.h>
+// Function designed for chat between client and server. 
+void func(int sockfd) 
 { 
-	int server_fd;//descriptor socket
-    int new_socket;
-    int valread; // uisng in read()
-    /*
-    struct sockaddr_in 
-    {
-    short            sin_family;   // e.g. AF_INET
-    unsigned short   sin_port;     // e.g. htons(3490)
-    struct in_addr   sin_addr;     // see struct in_addr, below
-    char             sin_zero[8];  // zero this if you want to
-    };
-    */
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	char *test = "Hello from server"; 
-	
-	// Creating socket file descriptor 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	//printf("server_fd : %d\n ",server_fd);
-	// Forcefully attaching socket to the port 8080 
-	//setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-    //printf("setsockopt : %d\n ",setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt
-                                                                              //  , sizeof(opt)));
+	char buff[MAX]; 
+	int n; 
+	// infinite loop for chat 
+	for (;;) 
+    { 
+		bzero(buff, MAX); 
 
-    address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
-	
-	// Forcefully attaching socket to the port 8080 
-	bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-	
-    //printf("bind: %d\n",bind(server_fd, (struct sockaddr *)&address, sizeof(address)));
-    //int listen(int sockfd, int backlog)
-    // backlog20192019:defines the maximum length to which the queue of pending connections for sockfd may grow
-	listen(server_fd, 3);
-	
-    //printf("listen : %d\n",listen(server_fd,3));
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen) ;
-    printf("Hello message sent\n"); 
-	valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-	send(new_socket , test , strlen(test) , 0 ); 
-	return 0; 
+		// read the message from client and copy it in buffer 
+		read(sockfd, buff, sizeof(buff)); 
+		// print buffer which contains the client contents 
+		printf("Message from client: %s\t To client : ", buff); 
+		bzero(buff, MAX); 
+		n = 0; 
+		// copy server message in the buffer 
+		while ((buff[n++] = getchar()) != '\n') 
+			; 
+
+		// and send that buffer to client 
+		write(sockfd, buff, sizeof(buff)); 
+
+		// if msg contains "Exit" then server exit and chat ended. 
+		if (strncmp("exit", buff, 4) == 0)  
+         { 
+			printf("Server Exit...\n"); 
+			break; 
+		 } 
+	} 
 } 
+
+// Driver function 
+int main() 
+{ 
+	int i=0;
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	bzero(&servaddr, sizeof(servaddr)); 
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(PORT); 
+	// Binding newly created socket to given IP and verification 
+	bind(sockfd, (SA*)&servaddr, sizeof(servaddr));
+	// Now server is ready to listen and verification 
+	listen(sockfd, 5);
+	len = sizeof(cli); 
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	// Function for chatting between client and server 
+	func(connfd); 
+	// After chatting close the socket 
+	close(sockfd); 
+} 
+
